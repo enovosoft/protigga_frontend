e2a7da1d414ed5420573de02f569ad4d36533f96
Tanzil Islam <tanzil.darkcode@gmail.com>
2025-10-04 18:48:08 +0600

    initial commit: added react,tailwindcss and shadcn also added fonts

---
+++ README.md
@@ -0,0 +1,3 @@
+# Protigga Webapp frontend
+
+Protigga is an ed tech platform of bangladesh
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="font-primary">প্রতিজ্ঞা - স্বপ্ন পূরণে</h2>
+
+      <h3 className="font-secondary">web app</h3>
+    </>
+  );
+}
+
+export default App;
